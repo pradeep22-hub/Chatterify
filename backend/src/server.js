f9291381cf0480402b489508acf5559d0d2d27d7
Pradeep Adhikari <info22pradeep@gmail.com>
2026-02-14 20:20:02 +0545

    worked on auth routes and auth controller

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import authRoutes from './routes/auth.route.js'
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+// middleware
+app.use(express.json())
+
+// routes
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
