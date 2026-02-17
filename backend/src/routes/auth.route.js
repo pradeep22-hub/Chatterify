7c087622d8400d91c2a781ceb9834c497a936a43
Pradeep Adhikari <info22pradeep@gmail.com>
2026-02-17 13:58:57 +0545

    Worked heavily on backend(almost completed)

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,12 +1,18 @@
-import express from 'express'
-import { login, logout, signup } from '../controllers/auth.controller.js'
+import express from "express";
+import { login, logout, onboard, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/signup', signup)
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
-router.post('/login', login)
+router.post("/onboarding", protectRoute, onboard);
 
-router.post('/logout', logout)
+// check if user is logged in
+router.get("/me", protectRoute, (req, res) => {
+  res.status(200).json({ success: true, user: req.user });
+});
 
-export default router
+export default router;
