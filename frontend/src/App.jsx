241a5affc13ae79abb8b8632b5bff179abdd1688
Pradeep Adhikari <info22pradeep@gmail.com>
2026-02-27 21:35:06 +0545

    Frontend updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { Navigate, Route, Routes } from "react-router";
+
+import { Navigate, Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/HomePage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
